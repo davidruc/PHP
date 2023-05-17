e6b2ea64944f69abba807f0611c0a45b80dd1018
davidruc <davidarueda18@gmail.com>
2023-05-17 09:17:31 -0500

    feat: :sparkles: estudio php
    se hizo un ejercio para calcular promedios

--- api.php
+++ api.php
@@ -1,15 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="main.js" defer></script>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+    <h1>Algoritmo promedios</h1>
+    <form  method="get">
+        <input type="text" name="nota1">
+        <input type="text" name="nota2">
+        <input type="text" name="nota3">
+        <input type="submit" value="respuesta">
+
+    </form>
+</body>
+</html>
+
 <?php
-    header("Content-type:application/json");
 
-    $config = (array) [];
-    $config ["http"]=(array)[];
-    $config ["http"]["method"]=(string) "GET";
-    $config ["http"]["header"]=(string) "Content-type:application/json";
-    $strm = stream_context_create($config);
-
-    $res = file_get_contents("https://pokeapi.co/api/v2/pokemon", false, $strm);
-
-    echo $res;
+if(isset($_GET['nota1']) && isset($_GET['nota2']) && isset($_GET['nota3']) && is_numeric($_GET['nota1']) && isset($_GET['nota2']) && isset($_GET['nota3']))
+{
+    $suma = $_GET['nota1'] + $_GET['nota2'] + $_GET['nota3'];
+    $promedio = $suma/3;
+    if($promedio <= 5 && $promedio > 3.9){
+        echo "Tu promedio fue excelente! :($promedio)";
+    } else if($promedio >= 0 && $promedio <= 3.9){
+        echo "Tu promedio fue muy malo >:c ($promedio)";
+    } else{
+        echo "como vas a sacar $promedio puta";
+    } 
+} else {
+    echo "subnormal";
+}
 
 ?>
 
--- main.js
+++
@@ -1,6 +0,0 @@
-let btn = document.querySelector("#btn");
-let uri = "http://localhost/SpUkM01-086/PHP"
-btn.addEventListener("click", async(e)=>{
-    let res = await (await fetch(`${uri}/api.php`)).json();
-    console.log(res);
-})
